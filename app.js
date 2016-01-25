c26174b289086374240f10ef2229ad4ccb2cf1a3
Cynthia Salinas <csalinas123@gmail.com>
2016-01-24 17:08:46 -0800

    add item features

--- app.js
+++ app.js
@@ -38,3 +38,14 @@ var purses = [
     }]
   }
 ]; 
+
+app.controller("PanelController", function(){
+  this.tab = 1; 
+
+  this.selectTab = function(setTab){
+    this.tab = setTab; 
+  }; 
+  this.isSelected = function(checkTab){
+    return this.tab ===checkTab; 
+  }; 
+}); 
